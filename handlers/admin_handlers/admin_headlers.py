97ec9b26b7b18c1dc475245ad86a84b1904ed9ff
pyadminrus <zh.vitaliy92@yandex.ru>
2023-11-17 21:20:48 +0300

    Улучшение кода

--- handlers/admin_handlers/admin_headlers.py
+++ handlers/admin_handlers/admin_headlers.py
@@ -8,71 +8,52 @@ from openpyxl import Workbook
 from system.dispatcher import dp, ADMIN_CHAT_ID  # Подключение к боту и диспетчеру пользователя
 
 
-# Функция для создания и отправки файла Excel с данными из базы данных
 async def send_data_as_excel(message: types.Message):
-    # Создаем временный файл Excel
-    excel_filename = "Зарегистрированные пользователи.xlsx"
-    # Создаем рабочую книгу Excel
-    wb = Workbook()
+    """Функция для создания и отправки файла Excel с данными из базы данных"""
+    excel_filename = "Зарегистрированные пользователи.xlsx"  # Создаем временный файл Excel
+    wb = Workbook()  # Создаем рабочую книгу Excel
     ws = wb.active
-    # Подключаемся к базе данных SQLite
-    conn = sqlite3.connect("your_database.db")
+    conn = sqlite3.connect("your_database.db")  # Подключаемся к базе данных SQLite
     cursor = conn.cursor()
-    # Выполняем SQL-запрос для извлечения данных
-    cursor.execute("SELECT * FROM users")
+    cursor.execute("SELECT * FROM users")  # Выполняем SQL-запрос для извлечения данных
     user_data = cursor.fetchall()
     # Заголовки столбцов
     ws.append(["User ID Telegram", "Имя", "Фамилия", "Город", "Номер телефона", "Дата регистрации"])
-    # Добавляем данные в таблицу
-    for row in user_data:
+    for row in user_data:  # Добавляем данные в таблицу
         ws.append(row)
-    # Сохраняем книгу Excel
-    wb.save(excel_filename)
-    # Закрываем соединение с базой данных
-    conn.close()
-    # Отправляем файл пользователю
-    with open(excel_filename, "rb") as excel_file:
+    wb.save(excel_filename)  # Сохраняем книгу Excel
+    conn.close()  # Закрываем соединение с базой данных
+    with open(excel_filename, "rb") as excel_file:  # Отправляем файл пользователю
         await message.reply_document(excel_file, caption="Данные зарегистрированных пользователей в боте")
-    # Удаляем временный файл Excel
-    os.remove(excel_filename)
+    os.remove(excel_filename)  # Удаляем временный файл Excel
 
 
 async def get_users_send_data_as_excel(message: types.Message):
-    # Создаем временный файл Excel
-    excel_filename = "Активные_пользователи.xlsx"
-    # Создаем рабочую книгу Excel
-    wb = Workbook()
+    excel_filename = "Активные_пользователи.xlsx"  # Создаем временный файл Excel
+    wb = Workbook()  # Создаем рабочую книгу Excel
     ws = wb.active
-    # Подключаемся к базе данных SQLite
-    conn = sqlite3.connect("your_database.db")
+    conn = sqlite3.connect("your_database.db")  # Подключаемся к базе данных SQLite
     cursor = conn.cursor()
-    # Выполняем SQL-запрос для извлечения данных
-    cursor.execute("SELECT * FROM users_start")
+    cursor.execute("SELECT * FROM users_start")  # Выполняем SQL-запрос для извлечения данных
     user_data = cursor.fetchall()
-    # Заголовки столбцов
-    ws.append(["User ID", "Username", "First Name", "Last Name", "Join Date"])
-    # Добавляем данные в таблицу
-    for row in user_data:
+    ws.append(["User ID", "Username", "First Name", "Last Name", "Join Date"])  # Заголовки столбцов
+    for row in user_data:  # Добавляем данные в таблицу
         ws.append(row)
-    # Сохраняем книгу Excel
-    wb.save(excel_filename)
-    # Закрываем соединение с базой данных
-    conn.close()
-    # Отправляем файл пользователю
-    with open(excel_filename, "rb") as excel_file:
+    wb.save(excel_filename)  # Сохраняем книгу Excel
+    conn.close()  # Закрываем соединение с базой данных
+    with open(excel_filename, "rb") as excel_file:  # Отправляем файл пользователю
         await message.answer_document(excel_file, caption="Данные зарегистрированных пользователей в боте")
-    # Удаляем временный файл Excel get_users   get_users_info
-    os.remove(excel_filename)
+    os.remove(excel_filename)  # Удаляем временный файл Excel get_users   get_users_info
 
 
-# Функция для проверки, является ли пользователь администратором
 def is_admin_user(user_id):
+    """Функция для проверки, является ли пользователь администратором"""
     return user_id == ADMIN_CHAT_ID
 
 
-# Обработчик команды /get_users
 @dp.message_handler(Command("get_users"))
 async def get_users_info(message: types.Message):
+    """Обработчик команды /get_users"""
     user_id = message.from_user.id
     if is_admin_user(user_id):
         await get_users_send_data_as_excel(message)
@@ -80,9 +61,9 @@ async def get_users_info(message: types.Message):
         await message.answer("Эта команда доступна только для администраторов.")
 
 
-# Обработчик команды /get_data
 @dp.message_handler(Command("get_data"))
 async def get_data_command(message: types.Message):
+    """Обработчик команды /get_data"""
     user_id = message.from_user.id
     if is_admin_user(user_id):
         await send_data_as_excel(message)
